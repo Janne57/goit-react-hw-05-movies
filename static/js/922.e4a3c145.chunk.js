b90d82bdb781ea3e995db98b8e0c62023c864a7c
Janne57 <121360302+Janne57@users.noreply.github.com>
2023-08-01 21:13:12 +0000

    Deploying to gh-pages from @ Janne57/goit-react-hw-05-movies@a6062c971cfab0c1158b9a688adf0d1abbc28ae5 🚀

---
+++ static/js/922.e4a3c145.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[922],{922:function(e,n,i){i.r(n);var t=i(87),s=i(184);n.default=function(){return(0,s.jsx)("main",{children:(0,s.jsx)("h1",{children:(0,s.jsx)(t.OL,{to:"/",children:"The page is not found"})})})}}}]);
+//# sourceMappingURL=922.e4a3c145.chunk.js.map
